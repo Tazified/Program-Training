24ab5bab882235df2f6422627b0fcc4d071d58ca
Tazified <111496801+Tazified@users.noreply.github.com>
2023-12-21 11:30:56 +0530

    Create InputSwap.c

---
+++ InputSwap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    printf("After swapping, num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
